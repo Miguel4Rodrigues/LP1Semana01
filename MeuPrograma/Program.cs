e26e8da080a884c82c4c8b79a68fb12f60e86c2a
Miguel Rodrigues <miguel4rodrigues@gmail.com>
2026-02-17 16:58:22 +0000

    Add /// comments

--- MeuPrograma/Program.cs
+++ MeuPrograma/Program.cs
@@ -2,8 +2,15 @@
 
 namespace MeuPrograma
 {
+    /// <summary>
+    /// Contains the main program execution.
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// prints "Hello LP!" and "Bye world"
+        /// </summary>
+        /// <param name="args"></param>
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello LP!");
