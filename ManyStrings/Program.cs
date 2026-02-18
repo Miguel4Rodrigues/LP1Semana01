09800e234e9f632e23a4463175a2573f77b494c5
Miguel Rodrigues <miguel4rodrigues@gmail.com>
2026-02-18 19:51:32 +0000

    Add ManyStrings

---
+++ ManyStrings/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+namespace ManyStrings
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            // variáveis string
+            Console.OutputEncoding = Encoding.UTF8;
+            string message = "So much programing skill";
+            string heart = "One heart \u2764";
+            string newLine = "\nStart \nEnd";
+            Console.WriteLine(message);
+            Console.WriteLine(heart);
+            Console.WriteLine(newLine);
+        }
+    }
+}
