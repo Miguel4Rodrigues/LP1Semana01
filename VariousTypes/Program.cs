ffdc3b6b5d6e62361ea7867f2a09123b1fe85cc8
Miguel Rodrigues <miguel4rodrigues@gmail.com>
2026-02-18 18:18:51 +0000

    Add VariousTypes

---
+++ VariousTypes/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace VariousTypes
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            byte num1 = 25;
+            short num2 = 2026;
+            int num3 = 1500000;
+            long num4 = 10000000000L;
+            Console.WriteLine(num1);
+            Console.WriteLine(num2);
+            Console.WriteLine(num3);
+            Console.WriteLine(num4);
+        }
+    }
+}
