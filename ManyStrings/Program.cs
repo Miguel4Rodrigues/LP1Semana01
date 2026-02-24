bdffbd73e5c720e7528bdd20240fefcfee2a687a
Miguel Rodrigues <miguel4rodrigues@gmail.com>
2026-02-24 15:21:08 +0000

    Add String formatting

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -16,18 +16,33 @@ namespace ManyStrings
             Console.WriteLine(heart);
             Console.WriteLine(newLine);
 
-            //variáveis criadas por concatenação
+            // variáveis criadas por concatenação
             string concat1 = "Hey" + 1;
             string concat2 = concat1 + 2;
             Console.WriteLine(concat1);
             Console.WriteLine(concat2);
-            //variáveis criadas por interpolação
+            // variáveis criadas por interpolação
             int camisolaPreco = 10;
             int camisolaQuantidade = 25;
             string inter1 = $"valor de camisola é {camisolaPreco}€";
             string inter2 = $"Disponíveis {camisolaQuantidade} Camisolas no total de {camisolaPreco * camisolaQuantidade}€";
             Console.WriteLine(inter1);
             Console.WriteLine(inter2);
+
+            // formatação de strings
+            double xx = 1.23456;
+            int ii = 19;
+
+            string xx1 = $"xx = {xx:f2}";
+            string xx2 = $"xx = {xx:p1}";
+            Console.WriteLine(xx1);
+            Console.WriteLine(xx2);
+
+            string ii1 = $"ii = {ii:x}";
+            string ii2 = $"ii = {ii:c}";
+            Console.WriteLine(ii1);
+            Console.WriteLine(ii2);
+
         }
     }
 }
