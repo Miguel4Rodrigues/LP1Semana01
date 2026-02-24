f168313b30af83c7c40d492de1ac3fd36133dada
Miguel Rodrigues <miguel4rodrigues@gmail.com>
2026-02-24 15:57:48 +0000

    Add ask for real number

--- AskForInput/Program.cs
+++ AskForInput/Program.cs
@@ -8,6 +8,8 @@ namespace AskForInput
         {
             Console.WriteLine("Insira um número inteiro: ");
             int numeroInteiro = int.Parse(Console.ReadLine());
+            Console.WriteLine("Insira um número real: ");
+            float numeroReal = float.Parse(Console.ReadLine());
         }
     }
 }
